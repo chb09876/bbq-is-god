328912664dcc7d0227ff75eb0249d93ebdc2212c
seon7129 <seon7129@naver.com>
2022-07-17 16:56:26 +0900

    week3

---
+++ seon7129/week3/selection_sort.c
@@ -0,0 +1,59 @@
+#include <stdio.h>
+
+void ft_swap(int *a, int *b)
+{
+    int c;
+    c = *a;
+    *a = *b;
+    *b = c;
+}
+
+void selection_sort(int *arr, int n)
+{
+    int i;
+    int j;
+    int min;
+
+    i = 0;
+    while (i < n)
+    {
+        min = i;
+        j = i + 1;
+        while (j < n)
+        {
+            if (arr[min] > arr[j])
+                min = j;
+            j++;
+        }
+        if (min != i)
+            ft_swap(&arr[i], &arr[min]);
+        i++;
+    }
+}
+
+int main()
+{
+    int n;
+    int arr[1000000];
+    int i;
+    int num;
+
+    scanf("%d", &n);
+    i = 0;
+    while(i < n)
+    {
+        scanf("%d", &num);
+        arr[i] = num;
+        i++;
+    }
+    selection_sort(arr, n);
+
+    i = 0;
+    while (i < n)
+    {
+        printf("%d\n", arr[i]);
+        i++;
+    }
+
+    return (0);
+}
